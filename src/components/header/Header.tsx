82527676a9810d5917487e4d57e711c5e53c42b0
Namhyun <jenple9709@gmail.com>
2024-03-31 18:51:28 +0900

    design: Implement Header design

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,39 @@
+import Image from 'next/image';
 import SearchBar from './components/searchbar/SearchBar';
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="bg-white flex">
-      <h1>Header</h1>
-      <SearchBar />
+    <header className="bg-white flex items-center mt-14 ">
+      <div className="flex items-center ml-9 space-x-10 font-semibold">
+        <Image
+          src="/큐아트 로고.png"
+          alt="큐아트 로고"
+          width={86}
+          height={35}
+        />
+        <Link href="exhibitions">전시회</Link>
+        <Link href="/artist">작가</Link>
+        <Link href="/artwork">작품</Link>
+      </div>
+
+      <div className="flex items-center space-x-4 ml-auto mr-16 gap-4">
+        <SearchBar />
+        <Link href="/login">로그인</Link>
+        <Link
+          href="/register"
+          className="flex bg-black text-white rounded-full py-2 px-4"
+        >
+          <Image
+            src="/qrcode.png"
+            alt="작품 등록"
+            width={20}
+            height={20}
+            className="fill-white flex"
+          />
+          작품 등록
+        </Link>
+      </div>
     </header>
   );
 }

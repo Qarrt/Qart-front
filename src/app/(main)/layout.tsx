4c12f072f53a51f858fe1be422ff5ef0563056a5
Namhyun <jenple9709@gmail.com>
2024-03-31 23:07:57 +0900

    chore: Directory changes

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/header/Header';
+import { ReactNode } from 'react';
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      <Header />
+      <main>{children}</main>
+    </div>
+  );
+}

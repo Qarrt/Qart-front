e8ee1ff550be64939fc099326df379d4009d408f
Namhyun <jenple9709@gmail.com>
2024-04-01 18:48:38 +0900

    design: Change margin

--- src/app/(main)/artwork/_component/ArtworkTab.tsx
+++ src/app/(main)/artwork/_component/ArtworkTab.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image';
 
 export default function ArtworkTab() {
   return (
-    <div className="flex ml-[199px] mt-[155px]">
+    <div className="flex ml-[199px] mt-[115px]">
       <Image src="/artistA.png" alt="artistA" width={129.6} height={129.6} />
       <div className="ml-[18px]">
-        <span className="flex flex-col font-bold">작가 한마디</span>
-        <span className="flex flex-col font-bold">김 현</span>
-        <span className="flex flex-col text-[#595959]">미술작가</span>
+        <span className="flex flex-col font-bold text-3xl">작가 한마디</span>
+        <span className="flex flex-col font-bold text-2xl mt-7">김 현</span>
+        <span className="flex flex-col text-xl text-[#595959] mt-1">
+          미술작가
+        </span>
       </div>
     </div>
   );

f9c9860b0fa04ec9ed6b506495c30232e7093298
Namhyun <jenple9709@gmail.com>
2024-04-02 19:34:00 +0900

    feat: Integrate react-hook-form with React Quill and enhance security with DOMPurifychore: Add types dompurify

--- src/app/(main)/upload-art/_component/ReactQuillTemplate.tsx
+++ src/app/(main)/upload-art/_component/ReactQuillTemplate.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import 'react-quill/dist/quill.snow.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
+import { useForm } from 'react-hook-form';
+import DOMPurify from 'dompurify';
 
 const QuillNoSSRWrapper = dynamic(() => import('react-quill'), {
-  ssr: false,
+  ssr: false, //클라이언트에서만 측에서만 렌더링되게 설정
   loading: () => <p>Loading ...</p>,
 });
 
@@ -47,6 +49,12 @@ export default function ReactQuillTemplate({
   height: string;
 }) {
   const [quillValue, setQuillValue] = useState('');
+  const { register, handleSubmit, setValue, trigger } = useForm({
+    mode: 'onChange',
+  });
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
 
   const handleQuillChange = (
     content: string,
@@ -54,21 +62,32 @@ export default function ReactQuillTemplate({
     source: any,
     editor: any,
   ) => {
-    setQuillValue(editor.getContents());
+    const sanitizedContent = DOMPurify.sanitize(editor.getHTML());
+    // 'content' 매개변수에는 에디터의 현재 HTML 내용이 담김
+    setValue(
+      'contents',
+      sanitizedContent === '<p><br></p>' ? '' : sanitizedContent,
+    );
+    trigger('contents'); // DOMPurify를 사용하여 공격에 취약한 코드를 제거
   };
+  useEffect(() => {
+    register('contents');
+  }, [register]);
 
   return (
     <>
-      <QuillNoSSRWrapper
-        className="custom-quill mt-[18px]"
-        style={{ width: '1040px', height: height }}
-        theme="snow"
-        modules={modules}
-        formats={formats}
-        value={quillValue || ''}
-        onChange={handleQuillChange}
-        placeholder={placeholder}
-      />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <QuillNoSSRWrapper
+          className="custom-quill mt-[18px]"
+          style={{ width: '1040px', height: height }}
+          theme="snow"
+          modules={modules}
+          formats={formats}
+          value={quillValue || ''}
+          onChange={handleQuillChange}
+          placeholder={placeholder}
+        />
+      </form>
     </>
   );
 }

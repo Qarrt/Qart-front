8669765d7adeca1ab7928b5cecf37168022f9d74
Namhyun <jenple9709@gmail.com>
2024-04-05 20:41:08 +0900

    refactor: useAxios baseURL

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const useAxios = () => {
   const axiosInstance = axios.create({
-    baseURL: 'https://localhost:8000',
+    baseURL: 'http://localhost:8000',
     headers: {
       'Content-Type': 'application/json',
     },

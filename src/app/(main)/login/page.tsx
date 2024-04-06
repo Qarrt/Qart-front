ba3f8cf77050a75d653998ad8e17fabbf272b149
Namhyun <jenple9709@gmail.com>
2024-04-06 19:32:20 +0900

    design: Initial design

--- src/app/(main)/login/page.tsx
+++ src/app/(main)/login/page.tsx
@@ -1,11 +1,23 @@
+import GoogleLoginButton from './google/page';
 import KakaoLoginButton from './kakao/page';
 import AfterLoginHeader from '@/components/header/AfterLoginHeader';
+import NaverLoginButton from './naver/page';
 
 export default function LoginPage() {
   return (
     <>
       <AfterLoginHeader />
-      <KakaoLoginButton />
+      <div className="flex flex-col items-center justify-center mt-[100px]">
+        <div className="mt-[40px]">
+          <KakaoLoginButton />
+        </div>
+        <div className="mt-[40px]">
+          <GoogleLoginButton />
+        </div>
+        <div className="mt-[40px]">
+          <NaverLoginButton />
+        </div>
+      </div>
     </>
   );
 }

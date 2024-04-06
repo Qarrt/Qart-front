9d4f666aba65735ef719c8b9e20f6759aac0b68d
Namhyun <jenple9709@gmail.com>
2024-04-06 19:32:42 +0900

    design: Initial design

--- src/app/(main)/login/kakao/page.tsx
+++ src/app/(main)/login/kakao/page.tsx
@@ -1,9 +1,15 @@
+import Image from 'next/image';
 export default function KakaoLoginButton() {
   return (
     <a
       href={`https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.KAKAO_CLIENT_ID}&redirect_uri=${process.env.BACKEND_URL}/auth/kakao`}
     >
-      카카오 로그인
+      <Image
+        src="/카카오로그인.svg"
+        alt="kakao login"
+        width={300}
+        height={150}
+      />
     </a>
   );
 }

e421ddd17d730d0ace8cc2c8b4ae2c64bd208e75
Namhyun <jenple9709@gmail.com>
2024-04-07 20:01:11 +0900

    refactor: Delete Header component

--- src/app/(main)/login/kakao/page.tsx
+++ src/app/(main)/login/kakao/page.tsx
@@ -4,12 +4,7 @@ export default function KakaoLoginButton() {
     <a
       href={`https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.KAKAO_CLIENT_ID}&redirect_uri=${process.env.BACKEND_URL}/auth/kakao`}
     >
-      <Image
-        src="/카카오로그인.svg"
-        alt="kakao login"
-        width={300}
-        height={150}
-      />
+      <Image src="/svg/kakao.svg" alt="kakao login" width={30} height={30} />
     </a>
   );
 }
--- src/app/(main)/login/page.tsx
+++ src/app/(main)/login/page.tsx
@@ -1,12 +1,10 @@
 import GoogleLoginButton from './google/page';
 import KakaoLoginButton from './kakao/page';
-import AfterLoginHeader from '@/components/header/AfterLoginHeader';
 import NaverLoginButton from './naver/page';
 
 export default function LoginPage() {
   return (
     <>
-      <AfterLoginHeader />
       <div className="flex flex-col items-center justify-center mt-[100px]">
         <div className="mt-[40px]">
           <KakaoLoginButton />
